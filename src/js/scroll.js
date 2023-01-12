82ccd7e5cd266a211761afd40816764798b3f8fc
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-01-13 00:27:40 +0100

    Update scroll.js

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,14 +1,35 @@
-export {scrollLoading, };
+export {scroll, scrollOnTop, scrollLoading};
 
+    //page scrolling up
 const btnUp = document.querySelector('.btnUp');
 
-const scrollLoading = (multiplayer) => {
+window.addEventListener('scroll', scroll);
+btnUp.addEventListener('click', scrollOnTop);
+
+const scroll = () => {
+    const scrolled = window.pageYOffset;
+    const cordination = document.documentElement.clientHeight;
+
+    if (scrolled > cordination) {
+        btnUp.style.display = 'block';
+    } else {
+        btnUp.style.display = 'none';
+    }
+    
+}
+
+const scrollOnTop = () => {
+    // this changes the scrolling behavior to "smooth"
+window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+const scrollLoading = () => {
     const { height: cardHeight } = document
   .querySelector(".gallery")
   .firstElementChild.getBoundingClientRect();
 
 window.scrollBy({
-  top: cardHeight * multiplayer,
+  top: cardHeight * 2,
   behavior: "smooth",
 });
 }
