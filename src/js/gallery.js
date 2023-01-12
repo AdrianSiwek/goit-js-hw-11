df64d9d248b9b90599993cb154f2d76041418220
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-01-13 00:45:51 +0100

    Create gallery.js

---
+++ src/js/gallery.js
@@ -0,0 +1,10 @@
+export { createCard };
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('gallery');
+
+const createCard = (galleryCard) => {
+    const newImages = galleryCard
+    .map()
+}
