cf8c3ed1f65d3118671bea5d9cdef8e8ee57cc05
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-01-15 22:30:30 +0100

    update

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import SimpleLightbox from "simplelightbox";
+import { scroll } from "./js/scroll";
+import {gallery} from "./js/gallery";
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,10 +1,25 @@
 export { createCard };
-import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('gallery');
 
 const createCard = (galleryCard) => {
     const newImages = galleryCard
-    .map()
+        .map(
+            <div class="photo-card">
+                <a href="${image.largeImageURL}">
+                    <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+      </a>
+                <div class="info">
+                    <p class="info-item"><b>Likes</b>${Image.likes}</p>
+                    <p class="info-item"><b>Views</b>${Image.views}</p>
+                    <p class="info-item"><b>Comments</b>${Image.comments}</p>
+                    <p class="info-item"><b>Downloads</b>${Image.dowloads}</p>
+                </div>
+            </div>
+        )
+        .join('');
+    
+    gallery.insertAdjacentHTML("beforeend", newImages)
 }
