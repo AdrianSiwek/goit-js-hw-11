97249a0ffd6172661dca5af00129ec5550e537e6
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-01-15 23:55:08 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import {scrollLoading} from "./js/scroll";
 import {createCard} from "./js/gallery";
 import Notiflix from 'notiflix';
 import { fetchPixabay } from "./js/fetchPixabay";
+
+
+const btnSearch = document.querySelector(".btnSearch");
+const inputSearch = document.querySelector(".search-form input");
+const btnDown = document.querySelector('.btn-down');
+const gallery = document.querySelector(".gallery");
+
