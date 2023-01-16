727fe248a7992f3ff61722e7d6a2c492b28fd9b0
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-01-16 23:45:57 +0100

    Update gallery.js

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -11,10 +11,10 @@ export const createCard = (galleryCard) => {
                     <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
       </a>
                 <div class="info">
-                    <p class="info-item"><b>Likes</b>${Image.likes}</p>
-                    <p class="info-item"><b>Views</b>${Image.views}</p>
-                    <p class="info-item"><b>Comments</b>${Image.comments}</p>
-                    <p class="info-item"><b>Downloads</b>${Image.dowloads}</p>
+                    <p class="info-item"><b>Likes</b>${image.likes}</p>
+                    <p class="info-item"><b>Views</b>${image.views}</p>
+                    <p class="info-item"><b>Comments</b>${image.comments}</p>
+                    <p class="info-item"><b>Downloads</b>${image.dowloads}</p>
                 </div>
             </div>`
         )
